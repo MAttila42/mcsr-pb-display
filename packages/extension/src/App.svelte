3a61e9f26db1a37b22da9f6508b07d3d10180a17
MAttila42 <65223119+MAttila42@users.noreply.github.com>
2025-09-09 02:30:10 +0200

    🔧 fix: hotfixes

--- packages/extension/src/App.svelte
+++ packages/extension/src/App.svelte
@@ -1,9 +1,8 @@
-<script lang="ts">
+<script lang='ts'>
   import '@unocss/reset/tailwind.css'
   import '@fontsource/ubuntu'
 </script>
 
-<main class="m-4">
-  <h1 class="text-xl font-bold">MCSR PB Display Extension</h1>
+<main class='m-4'>
+  <h1 class='text-xl font-bold'>MCSR PB Display Extension</h1>
 </main>
-
