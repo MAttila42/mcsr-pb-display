87d80839f9f0d2ec7a29c8aedc894a18464af688
MAttila42 <65223119+MAttila42@users.noreply.github.com>
2025-09-09 02:55:19 +0200

    🔧 fix(wip): cloudlfare worker throws 500

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -1,6 +1,10 @@
 import { Elysia } from 'elysia'
 import { records } from './routes/records'
 
-export const app = new Elysia()
+export const app = new Elysia({ aot: false })
+  .onError(({ code, error }) => {
+    console.error(code)
+    return new Response(JSON.stringify({ error: error.toString() ?? code }), { status: 500 })
+  })
   .get('/', () => 'Hello Elysia')
   .use(records)
