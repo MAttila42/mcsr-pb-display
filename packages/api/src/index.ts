e815d82cf681063d78e5473905ffa6ec5df68d31
MAttila42 <65223119+MAttila42@users.noreply.github.com>
2025-09-11 21:29:04 +0200

    🔧 fix: cloudflare error

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,9 +1,8 @@
 import type { Env } from 'bun'
 import type { Context } from 'elysia'
 import { cors } from '@elysiajs/cors'
-import { staticPlugin } from '@elysiajs/static'
 import { createHandler } from '@rttnd/gau/core'
-import { Elysia } from 'elysia'
+import { Elysia, file } from 'elysia'
 import { auth } from './auth'
 import { records } from './routes/records'
 
@@ -15,8 +14,11 @@ const app = new Elysia({
 })
   .mount(handler)
   .use(cors())
-  .use(staticPlugin({ prefix: '/' }))
   .use(records)
+  .get(
+    '/.well-known/microsoft-identity-association.json',
+    file('./public/.well-known/microsoft-identity-association.json'),
+  )
   .get('/auth-success', 'Account link was successful! You can close this tab.')
   .get('/', 'This is the backend API for the MCSR PB Display extension. No content here.')
 
