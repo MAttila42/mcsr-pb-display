d45d208dbe3cd9fa707d85578c2ad5c4c833c4d6
MAttila42 <65223119+MAttila42@users.noreply.github.com>
2025-09-25 11:37:41 +0200

    ➖ remove: unnecessary background script

--- packages/extension/vite.config.ts
+++ packages/extension/vite.config.ts
@@ -13,7 +13,6 @@ export default defineConfig({
       input: {
         popup: resolve(__dirname, './popup.html'),
         content: resolve(__dirname, './src/content.ts'),
-        background: resolve(__dirname, './src/background.ts'),
       },
       output: {
         entryFileNames: '[name].js',
