3f8019e958e86b0a15e545879bd6bd3464c103f0
MAttila42 <65223119+MAttila42@users.noreply.github.com>
2025-09-28 01:56:29 +0200

    🔧 fix: search button icon

--- packages/extension/src/lib/components/Search.svelte
+++ packages/extension/src/lib/components/Search.svelte
@@ -58,7 +58,7 @@
         type='submit'
         disabled={lookupLoading}
       >
-        <div class='i-fluent:chevron-right-12-filled'>&#62;</div>
+        <div class='i-fluent-search-12-filled size-4'></div>
       </Button>
     </form>
     {#if lookupError}
